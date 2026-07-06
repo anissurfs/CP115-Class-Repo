3eb52fdac01ce4562ddd61778f92233761253809
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-07-06 09:32:34 +0800

    aupdate ex3

--- labs/lab04/exercise4/lab-4-4.py
+++ labs/lab04/exercise4/lab-4-4.py
@@ -1,6 +1,4 @@
-print("Input weight")
 weight = float(input())
-print("Input Ticket Price")
 ticketPrice = float(input())
 if weight == 0:
     finalPrice = ticketPrice - 10
@@ -8,6 +6,6 @@ else:
     if weight <= 15:
         pass
     else:
-        exWeight = (weight - 15) * 4
+        exWeight = weight - 15 * 4
         finalPrice = exWeight + ticketPrice
 print(finalPrice)
