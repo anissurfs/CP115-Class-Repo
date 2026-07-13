025e3799d973a5c07701287f1f9a65d0b173ee45
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-07-13 08:12:10 +0800

    ex1-3

--- labs/lab04/exercise1/lab-4-1.py
+++ labs/lab04/exercise1/lab-4-1.py
@@ -1,9 +1,9 @@
 kwh = float(input())
 if kwh >= 100:
     if kwh >= 200:
-        totalBill = 80 + kwh - 200 * 0.75
+        totalBill = 80 + ((kwh - 200) * 0.75)
     else:
-        totalBill = 30 + kwh - 100 * 0.5
+        totalBill = 30 + ((kwh - 100) * 0.5)
 else:
     totalBill = kwh * 0.3
 print(totalBill)
