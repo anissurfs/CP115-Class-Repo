addc8496ae776c8ea1ac3063d54de002088ee403
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-07-13 08:22:04 +0800

    ex2-2

--- labs/lab04/exercise2/lab-4-2.py
+++ labs/lab04/exercise2/lab-4-2.py
@@ -1,9 +1,9 @@
 income = float(input())
 if income >= 50000:
     if income >= 100000:
-        totalTax = 500 + income - 100000 * 0.02
+        totalTax = 500 + ((income - 100000) * 0.02)
     else:
-        totalTax = income - 50000 * 0.01
+        totalTax = ((income - 50000) * 0.01)
 else:
     totalTax = 0
 print(totalTax)
