e90f7bd51c86d69633cb43945bcbbedaf9b9d46d
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-08-10 08:35:15 +0800

    lab6

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -1,13 +1,14 @@
-print("Hello World how are you?")
-
-print("Hello\nWorld\nHow are you?")
-
-message = "Name: Ali\nAge : 20\nGrade: A"
-print(message)
-
-print ("Name Age Grade")
-print ("Ali 20 A")
-
-print ("Name\tAge\tGrade")
-print ("Ali\t20\tA")
-print ("Sarah\t19\tB+")
+coffeeP = 3.50
+muffinP = 2.10
+waterP = 1.05
+coffeeQ = int(input("Coffee Quantity: "))
+muffinQ = int(input("Coffee Quantity: "))
+waterQ = int(input("Coffee Quantity: "))
+coffeeT = coffeeP * coffeeQ
+muffinT = muffinP * muffinQ
+waterT = waterP * waterQ
+subtotal = coffeeT + muffinT + waterT
+tax = subtotal * 0.06
+totalP = subtotal + tax
+print(coffeeT)
+print(f"========== RECEIPT ==========\nItem\tPrice\tQty\tTotal\nCoffee\t${coffeeP}\t{coffeeQ}\t${round(coffeeT,2)}\nMuffin\t${muffinP}\t{muffinQ}\t${round(muffinT,2)}\nWater\t${waterP}\t{waterQ}\t${round(waterT,2)}\n-----------------------------\nSubtotal\t\t${round(subtotal,2)}\nTax (6%)\t\t${round(tax,2)}\nTotal\t\t\t${round(totalP,2)}\n=============================")
