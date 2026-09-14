f6890f68b47df97bae79c6d7a7319ea4158f7325
qvreeyee-ctrl <qvreeyee@gmail.com>
2026-09-14 09:33:10 +0800

    ex6

--- labs/lab08/exercise6/exercise6.py
+++ labs/lab08/exercise6/exercise6.py
@@ -2,6 +2,11 @@ position = input()
 overtime_hours = int(input())
 is_weekend = input()
 
-
+if position == "Manager":
+    base_rate = 30
+elif position == "Supervisor":
+    base_rate = 20
+elif position == "Staff":
+    base_rate = 
 
 print(overtime_pay)
