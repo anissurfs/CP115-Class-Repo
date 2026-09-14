c0fbcd47455c78cafeda45aa2de34ff85942a29e
anissoleha <anissolehasallehuddin@gmail.com>
2026-09-14 10:53:30 +0800

    ex6

--- labs/lab08/exercise6/exercise6.py
+++ labs/lab08/exercise6/exercise6.py
@@ -7,6 +7,16 @@ if position == "Manager":
 elif position == "Supervisor":
     base_rate = 20
 elif position == "Staff":
-    base_rate = 
+    base_rate = 15
+else:
+    base_rate = 8
+
+if overtime_hours <= 8:
+    overtime_pay = overtime_hours * (base_rate * 1.5)
+else:
+    overtime_pay = (8 * (base_rate * 1.5)) + ((overtime_hours - 8) * 2)
+
+if is_weekend == "yes":
+    overtime_pay = overtime_pay + (overtime_hours * 5)
 
 print(overtime_pay)
